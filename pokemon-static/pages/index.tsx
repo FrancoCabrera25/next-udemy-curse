1a1b4013111cf15cc42baf2b6605b8ec77bcfb4e
Franco Cabrera <64378629+FrancoCabrera25@users.noreply.github.com>
2022-03-06 20:09:13 -0300

    initial navbar

--- pokemon-static/pages/index.tsx
+++ pokemon-static/pages/index.tsx
@@ -1,14 +1,14 @@
-import type { NextPage } from 'next'
-import { Button } from '@nextui-org/react';
-
+import type { NextPage } from "next";
+import { Button } from "@nextui-org/react";
+import { Layout } from "../components/layouts";
 
 const HomePage: NextPage = () => {
   return (
-        <>
-        <h1>Hola mundo</h1>
-        <Button color='gradient'>Click me</Button>
-        </>
-  )
-}
+    <Layout title="Listado de pokemon">
+      <h1>Hola mundo</h1>
+      <Button color="gradient">Click me</Button>
+    </Layout>
+  );
+};
 
-export default HomePage
+export default HomePage;
