344e7829ab79486b23fe652c2efeccd2c880f541
Franco Cabrera <64378629+FrancoCabrera25@users.noreply.github.com>
2022-03-06 21:51:01 -0300

    api

--- pokemon-static/pages/index.tsx
+++ pokemon-static/pages/index.tsx
@@ -1,8 +1,11 @@
-import type { NextPage } from "next";
+import type { NextPage, GetStaticProps } from "next";
 import { Button } from "@nextui-org/react";
 import { Layout } from "../components/layouts";
+import { pokeApi } from "../api";
+import { PokemonListResponse } from "../interfaces";
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+  console.log(props);
   return (
     <Layout title="Listado de pokemon">
       <h1>Hola mundo</h1>
@@ -10,5 +13,19 @@ const HomePage: NextPage = () => {
     </Layout>
   );
 };
+
+
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+
+  const {data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');  
+
+  return {
+    props: {
+      pokemons: data.results
+    }
+  }
+}
+
 
 export default HomePage;
